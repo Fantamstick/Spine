22cd46f25a414482b1bbf121bcc8a3f658ad739f
Eishin Suga <cyberfox1002@gmail.com>
2024-09-20 14:31:40 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Spine",
     platforms: [
-        .iOS(.v8), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v3)
+        .iOS(.v13), .macOS(.v10_10), .tvOS(.v9), .watchOS(.v3)
     ],
     products: [
         .library(name: "Spine", targets: ["Spine"]),
